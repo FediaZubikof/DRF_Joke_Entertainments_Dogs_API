c871c5fc94d626cb7ec58ff585cc4b3305a6be99
Fiodar Zubikau <136503036+FediaZubikof@users.noreply.github.com>
2023-11-14 18:34:40 +0300

    Добавлены первоначальные методы get

--- project/joke/views.py
+++ project/joke/views.py
@@ -2,8 +2,9 @@ import requests
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import viewsets
 
-from .models import Joke, Entertainments
+from .models import Joke, Entertainments, DogsPhoto
 from .serializers import JokeSerializer, EntertainmentsSerializer, DogsPhotoSerializer
 from django.conf import settings
 
@@ -14,7 +15,6 @@ class BaseViewSet(APIView):
     api_url = None
 
     def post(self, request):
-
         response = requests.get(self.api_url)
         self.serializer_class.save_response(response.json(), response_data=response.json())
         return Response(response.json(), status=response.status_code)
@@ -34,3 +34,29 @@ class DogsPhotoViewSet(BaseViewSet):
     serializer_class = DogsPhotoSerializer
     api_url = settings.URL_PHOTO
 
+
+class RandomPhotoViewSet(APIView):
+    serializer_class = DogsPhotoSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        photo = DogsPhoto.objects.all().values()
+        return Response(photo)
+
+
+class RandomEntertainmentsViewSet(APIView):
+    serializer_class = EntertainmentsSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        enter = Entertainments.objects.all().values()
+        return Response(enter)
+
+
+class RandomJokeViewSet(APIView):
+    serializer_class = JokeSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        joke = Joke.objects.all().values()
+        return Response(joke)
